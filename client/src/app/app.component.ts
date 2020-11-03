a2f7457a9a8ed412bf334ebdf98b606ddbe15116
Carlos Diaz <carlosfdiazramos@gmail.com>
2020-11-03 17:03:35 -0600

    started building shop component - layout items - layout filters

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+
 
 @Component({
   selector: 'app-root',
@@ -10,17 +8,9 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:44338/api/products?pageSize=50').subscribe((response: IPagination) => {
-      console.log(response);
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
-  }
+  ngOnInit(): void { }
 
 }
